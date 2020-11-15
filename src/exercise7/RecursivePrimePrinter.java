8668452275b9096b83301288e1dc223d88b8c4e7
doshev17 <stefan.doshev@mail.bg>
2020-11-15 14:29:14 +0000

    c++

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -1,6 +1,7 @@
 package exercise7;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class RecursivePrimePrinter {
     public static void main(String[] args) {
@@ -13,11 +14,23 @@ public class RecursivePrimePrinter {
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
+
+        if(n==1) {System.out.println(n);}
+        else {
+            if(isPrime(n)) System.out.println(n);
+            printPrimes(n-1);
+        }
+
     }
 
     public static boolean isPrime(int n) {
-        // Implement method
-        return false;
+
+        boolean prime=true;
+        for(int i=2;i<n;i++){
+            if(n%i ==0){
+                prime=false;
+            }
+        }
+        return prime;
     }
 }
